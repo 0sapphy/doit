ea3afcb3e4ccdbda4700cb463e7c3ca855ec07a1
S🅰️pph <138983011+0sapphy@users.noreply.github.com>
2025-04-27 21:19:25 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -301,12 +301,10 @@ app.delete("/api/todos/:id", isAuthenticated, async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
+/*app.listen(3000, () => {
   console.log("Server is running!");
-});
-
-const server = app;
+});*/
 
 module.exports = (req, res) => {
-  return server(req, res); 
+  return app(req, res);
 };
