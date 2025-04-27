cdb32e62589176fa35b409abfe568c7c7810aa2d
S🅰️pph <138983011+0sapphy@users.noreply.github.com>
2025-04-27 20:54:15 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-require("node:process").loadEnvFile();
+require("node:process").loadEnvFile().catch()
 const path = require("node:path");
 const express = require("express");
 const mongoose = require("mongoose");
